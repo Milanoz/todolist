2138052ec265faf134927304f77b76d27da561ef
Nguyễn Văn Huy <104371651+Milanoz@users.noreply.github.com>
2023-06-14 08:24:15 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.scss';
 import ModalAdd from './component/ModalAdd';
+import useModal from './useReact/useModalAdd';
 
 function App() {
+
+  const {isShowing, toggle} = useModal();
+
   return (
     <div className="App">
       <header className="App-header">
-        <ModalAdd/>
+      <button className="button-default" onClick={toggle}>Show Modal</button>
+      <ModalAdd
+        isShowing={isShowing}
+        hide={toggle}
+      />
       </header>
     </div>
   );
--- src/component/ModalAdd.js
+++ src/component/ModalAdd.js
@@ -1,25 +1,26 @@
-function ModalAdd(props)
-{
-    return(
-        <>
-            <div className="Modal">
-                <div className="Modal-dialog">
-                    <div className="Modal-content">
-                        <div className="Modal-header">
-                            <h3 className="Modal-title">Create new task</h3>
-                        </div>
-                        <div className="Modal-body">
-                            <input type="text" className="Add-input"/>
-                        </div>
-                        <div className="Modal-footer">
-                            <button className="btn-save">Save</button>
-                            <button className="btn-close">Close</button>
-                        </div>
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ModalAdd = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
+  <>
+    <div className="Modal">
+            <div className="Modal-dialog">
+                <div className="Modal-content">
+                    <div className="Modal-header">
+                        <h3 className="Modal-title">Create new task</h3>
+                    </div>
+                    <div className="Modal-body">
+                        <input type="text" className="Add-input" placeholder="Enter your task"/>
+                    </div>
+                    <div className="Modal-footer">
+                        <button className="btn-save">Save</button>
+                        <button className="btn-close">Close</button>
                     </div>
                 </div>
             </div>
-        </>
-    )
-}
+        </div>
+  </>, document.body
+) : null;
 
-export default ModalAdd
+export default ModalAdd;
+
